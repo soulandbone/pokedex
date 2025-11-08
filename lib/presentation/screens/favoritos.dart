d9fe061af291f12ec95c4a655b7b70498661397d
Gustavo G <soulandbone@gmail.com>
2025-11-08 19:52:33 -0300

    Now the favorites are shown on the favorites tab

--- lib/presentation/screens/favoritos.dart
+++ lib/presentation/screens/favoritos.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pokedex/presentation/widgets/pokemon_tile.dart';
+import 'package:pokedex/providers/favorites.dart';
+import 'package:pokedex/providers/providers.dart';
 
-class Favoritos extends StatelessWidget {
+class Favoritos extends ConsumerWidget {
   const Favoritos({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('Este es el favoritos')),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final allPokemons = ref.watch(pokemonListProvider);
+    final favorites = ref.watch(favoritesProvider);
+    final filteredPokemons = allPokemons.when(
+      data: (allPokemons) =>
+          allPokemons.where((p) => favorites.contains(p.id)).toList(),
+      loading: () => [],
+      error: (e, st) => [],
+    );
+    return ListView.builder(
+      itemCount: filteredPokemons.length,
+      itemBuilder: (context, index) {
+        final pokemon = filteredPokemons[index];
+        return PokemonTile(pokemon: pokemon);
+      },
     );
   }
 }
