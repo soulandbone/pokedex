1e549c90d6cec83ea2042998a730aedc5d98f2a5
Gustavo G <soulandbone@gmail.com>
2025-11-11 21:40:06 -0300

    small changes for flow of the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:pokedex/presentation/screens/home_page.dart';
 import 'package:pokedex/presentation/screens/onboarding_1.dart';
 
 void main() {
